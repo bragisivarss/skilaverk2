8d8780a60b6beba2761cfa4c54e08904f19ad2c4
bragisivarss <sigursteinnbragi@gmail.com>
2023-10-05 10:09:33 +0000

    fixes

--- Verkefni/Verk2.dart
+++ Verkefni/Verk2.dart
@@ -1,11 +1,9 @@
 import 'dart:io';
 
 void main(){
-  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  print("Please enter a number:");
-  String? input = stdin.readLineSync();
-  double numb = double.parse(input!);
+  stdout.write("Please enter a number: ");
+  double numb = double.parse(stdin.readLineSync() ?? '0');
 
   for(int i = 1; i <= 10; i++){
     double result = numb * i;
